224add7dde0a2d0d05f0a3c04199d6e85424bd3d
Fernando Pires da Cunha <94570027+fernandopcunha281085@users.noreply.github.com>
2022-03-17 20:41:38 -0300

    m

--- php/index.php
+++ php/index.php
@@ -1,1 +1,18 @@
-<h1>Olá tudo bem com você?</h1>
+<?php
+$ingrediente1 = 'açucar';
+$ingrediente2 = 'farinha de trigo';
+$ingrediente3 = 'ovo';
+
+$ingredientes = [
+    'açucar',
+    'farinha de trigo', 
+    'ovo', 
+    'leite', 
+    'fermento em pó'
+];
+
+
+
+$numerosDaMegaSena = [10, 14, 27, 34, 8];
+echo "Número 1: ". $numerosDaMegaSena[0]."<br/>";
+echo "Número 2: ". $numerosDaMegaSena[1];
