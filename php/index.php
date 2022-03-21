2125906cdd87c54072e8af36c6c8aee0ee880d3a
Fernando Pires da Cunha <94570027+fernandopcunha281085@users.noreply.github.com>
2022-03-21 10:24:21 -0300

    m

--- php/index.php
+++ php/index.php
@@ -1,15 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF8" />
+        <title>Omega Informática e Copiadora</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="projetocurso.css" />
+    </head>
+    <body>
 <?php
-$bolo1 = [
-    'açucar',
-    'farinha de trigo', 
-    'ovo', 
-    'leite', 
-    'fermento em pó'
-];
 
-$bolo2 = [
-    ...$bolo1,
-    'corante'
-];
-
-echo $bolo2[3];
+$door = 5;
+switch($door) {
+    case '1': 
+        echo "Você está entrando pela porta errada.";
+    break;
+    case '2': 
+        echo "Você está entrando por uma porta certa.";
+        break;
+    case '3': 
+            echo "Você está entrando por uma porta certa.";
+            break;
+        default: 
+       echo "Você não escolheu nenhuma porta válida."; 
+        break;
+    }
+?>
+</body>
+</html> 
