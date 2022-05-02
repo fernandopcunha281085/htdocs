e8020736c2eb9e5e64620811098c67edd1531b49
Fernando <fernandopcunha281085@gmail.com>
2022-05-02 14:56:48 -0300

    PHP

--- php/index.php
+++ php/index.php
@@ -1,29 +1,33 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF8" />
-        <title>Omega Informática e Copiadora</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="projetocurso.css" />
-    </head>
-    <body>
 <?php
+$data = '2020-05-06';
+$semana = date('w', strtotime($data));
 
-$door = 5;
-switch($door) {
-    case '1': 
-        echo "Você está entrando pela porta errada.";
-    break;
-    case '2': 
-        echo "Você está entrando por uma porta certa.";
-        break;
-    case '3': 
-            echo "Você está entrando por uma porta certa.";
-            break;
-        default: 
-       echo "Você não escolheu nenhuma porta válida."; 
-        break;
-    }
-?>
-</body>
-</html> 
+switch ($semana) {
+ case '0':
+ echo "Domingo";
+ break;
+ case '1':
+ echo "Segunda";
+ break;
+ case '2':
+ echo "Terça";
+ break;
+ case '3':
+ echo "Quarta";
+ break;
+ case '4':
+ echo "Quinta";
+ break;
+ case '5':
+ echo "Sexta";
+ break;
+ case '6':
+ echo "Sábado";
+ break;
+ case '7':
+ echo "Domingo";
+ break;
+ default:
+ echo "ERRO!";
+ break;
+}
